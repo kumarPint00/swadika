38d524180c94acce010560b74af3756f9a1b1ce0
kumarpint00 <kumarpintoo417@gmail.com>
2025-06-18 14:47:32 +0530

    chore: comment out InstagramFeed component in TestimonialDeck

--- components/TestimonialDeck.tsx
+++ components/TestimonialDeck.tsx
@@ -68,7 +68,7 @@ export default function TestimonialDeck() {
           boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
         }}
       />
-      <InstagramFeed />
+      {/* <InstagramFeed /> */}
     </Box>
   );
 }
