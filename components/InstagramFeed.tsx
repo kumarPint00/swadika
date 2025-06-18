a0a80ecfdf4d7808c6a8efd75fe99c1f77009e41
kumarpint00 <kumarpintoo417@gmail.com>
2025-06-18 15:13:57 +0530

    refactor: update InstagramFeed component to load script directly in JSX

--- components/InstagramFeed.tsx
+++ components/InstagramFeed.tsx
@@ -1,19 +1,17 @@
 "use client";
-import { useEffect } from "react";
 
 export default function InstagramFeed() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const s=document.createElement("script");
-      s.src="https://cdn2.woxo.tech/a.js"; s.async=true;
-      document.body.appendChild(s);
-    }
-  }, []);
   return (
     <div
       className="woxo-instagram-feed"
       data-widget-id="YOUR_WOXO_WIDGET_ID"
       style={{ width: "100%", overflow: "hidden" }}
-    />
+    >
+      <script
+        src="https://cdn2.woxo.tech/a.js"
+        async
+        defer
+      />
+    </div>
   );
 }
